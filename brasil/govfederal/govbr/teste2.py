673586f7918ce2baec45048b993fe4125045f0b7
João Lucas Passos Motta <darthmotta@gmail.com>
2021-05-06 20:10:59 +0000

    Replace teste2.py

--- brasil/govfederal/govbr/teste2.py
+++ brasil/govfederal/govbr/teste2.py
@@ -1,13 +1,15 @@
 import csv
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import re
+import requests
 
 csvFile = open("C:/Users/Pichau/codigo/govbr/brasil/govfederal/govbr/arquivos/teste.txt",'wt')
 paginas = set()
 def getLinks(pageUrl):
     global paginas
-    html = urlopen("https://www.gov.br/pt-br/"+pageUrl)
+    url = requests.get("https://www.gov.br"+pageUrl, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'})
+    html = url.text
     bsObj = BeautifulSoup(html, "html.parser")
     writer = csv.writer(csvFile)
     for link in bsObj.findAll("a"):
