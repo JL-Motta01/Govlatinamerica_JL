ce6d5ddbdeba0bfecdee71b355fe5ccee6f6fbf6
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2021-06-01 21:00:53 -0300

    add script coleta discursos de posse mre funag

--- brasil/govfederal/govbr/teste3.py
+++ brasil/govfederal/govbr/teste3.py
@@ -1,17 +1,4 @@
-from scrapy.spiders import CrawlSpider
-from scrapy import Request
-from urllib.parse import urljoin
 
-class MySpider(CrawlSpider):
-    name = 'myspider'
-    start_urls = ['https://www.gov.br']
-
-    def parse(self, response):
-        # This method is called for every successfully crawled page
-
-        # get all pagination links using xpath
-        for link in response.xpath('.//a/@href').getall():
-            # build the absolute url 
-            url = urljoin('https://www.gov.br', link)
-            print(url)
-            yield Request(url=url, callback=self.parse)  # <-- This makes your spider recursiv crawl subsequent pagespip
+import importlib
+module = importlib.load_module('/home/lantri_rafael/codigo/govlatinamerica/brasil/govfederal/mre.funag_ministro.py')
+module.function()
