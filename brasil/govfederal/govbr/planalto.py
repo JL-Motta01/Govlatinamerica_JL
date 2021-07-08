7ee7bc99f6ee58c6dcd66cb1204f7743c56e0d4d
JLMotta <darthmotta@gmail.com>
2021-07-08 12:34:40 -0300

    Atualização de leitura do sitemap, pegamdo o sitemap mais atualizado diretamente da fonte

--- brasil/govfederal/govbr/planalto.py
+++ brasil/govfederal/govbr/planalto.py
@@ -3,11 +3,16 @@ import urllib.parse
 import lxml
 from bs4 import BeautifulSoup
 import pandas as pd
+import csv
 
-
-url = 'C:/Users/Pichau/codigo/govlatinamerica/brasil/govfederal/govbr/arquivos/sitemap.xml'
+csvFile = open("C:/Users/Pichau/codigo/govlatinamerica/brasil/govfederal/govbr/arquivos/testecsv.txt",'wt')
+url = 'https://www.gov.br/sitemap.xml'
+writer = csv.writer(csvFile)
 with open(url,"r") as page:
     bs=BeautifulSoup(page,"lxml")
     sitemap = bs.find_all("url")
     for link in sitemap:
         print (link.find("loc").get_text())
+        csvRow = []
+        csvRow.append(link.find("loc").get_text())
+        writer.writerow(csvRow)
