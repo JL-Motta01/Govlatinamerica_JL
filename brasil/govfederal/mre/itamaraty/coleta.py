41ed215ab0286a20ffd235e9e7bbb529064e6f3e
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2021-10-01 12:46:39 -0300

    inicio coleta itamaraty

--- brasil/govfederal/mre/itamaraty/coleta.py
+++ brasil/govfederal/mre/itamaraty/coleta.py
@@ -2,21 +2,19 @@ from urllib.request import build_opener, urlopen  # biblioteca nativa
 from bs4 import BeautifulSoup  # biblioteca de terceiros
 
 
-def pagina():
+def nota_imprensa():
+    mre_acesso_pagina = acesso_pagina()
+    lista_paginas = []
+
+
+def acesso_pagina():
     html = urlopen("http://www.biblioteca.presidencia.gov.br/")
-    # chama a página
     bs = BeautifulSoup(html, "html.parser")
-    # percorre os elementos que queremos
     return bs
-
-
-def links_noticias(bs):
-    pass
 
 
 def main():
     bs = pagina()
-    ## links = links_noticias(bs)
     print(bs)
 
 
