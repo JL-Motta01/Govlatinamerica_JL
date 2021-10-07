3ef36367c0f0b553ce23083577d5995649b6f757
JLMOTTA <darthmotta@gmail.com>
2021-10-07 02:22:56 -0300

    começo da base de dados para discursos

--- brasil/govfederal/govbr/coleta_discursos.py
+++ brasil/govfederal/govbr/coleta_discursos.py
@@ -11,6 +11,10 @@ from bs4 import BeautifulSoup #importa o beautifulsoup para extrair as infos das
 from pprint import pprint #organizar estéticamente os prints
 from requests.models import DecodeError 
 
+DIR_LOCAL= "/home/labri_joaomotta/codigo"
+
+DIR_DADOS= "/media/hdvm10/bd/003/001/001/001/001-b"
+
 def acesso_discursos (url):
     """Analisa os discursos do site a partir do link"""
     global response
@@ -41,12 +45,34 @@ def coleta_link_discursos ():
             lista_links.append(links)
     return lista_links
 
+def base_dados(xml):
+    db = TinyDB(f"{DIR_LOCAL}/govlatinamerica/brasil/govfederal/govbr/bd/db_discursos.json")
+    User = Query()
+    lista_geral = coleta_conteudo_discursos(xml)
+    for sublista in lista_geral:
+        db_planalto = db.contains(User.titulo==sublista[0])
+        if not db_planalto:
+            print("não está na base")
+            db.insert({
+                "link": sublista[0],
+                "data":sublista[1],
+                "atualizado em": sublista[2],
+                "categoria": sublista[3],
+                "tags": sublista[4],
+                "titulo": sublista[5],
+                "conteudo": sublista[6],
+            })
+            link_news = sublista[0]
+        else:
+            print("está na base")
+
+
 def coleta_conteudo_discursos ():
     """Responsável por coletar título, parágrafo, Tags, atualização e data dos discursos"""
     for link in coleta_link_discursos():
         discursos = acesso_discursos(link)
-        titulo = discursos.find("h1", class_="documentFirstHeading").text
-        print(titulo)
+        url = link
+        print(url)
         publicado_em = discursos.find("span", class_="documentPublished").find("span", class_="value").text
         print(publicado_em)
         try:
@@ -54,6 +80,17 @@ def coleta_conteudo_discursos ():
             print(atualizado_em)
         except:
             print("discurso não modificado")
+        try:
+            lista_tags=[]
+            spans=discursos.find("div", {"id" : "category"}).find_all("span") 
+            for span in spans:
+                tag = span.text
+                lista_tags.append(tag)
+            print(lista_tags)
+        except:
+            print("não possui tags")
+        titulo = discursos.find("h1", class_="documentFirstHeading").text
+        print(titulo)
         try:
             lista_conteudo=[]
             paragrafos = discursos.find('div', {'id' : 'content-core'}).find_all(['p','h3','h2','h1'])
@@ -67,14 +104,6 @@ def coleta_conteudo_discursos ():
         except:
             lista_conteudo= "notícia sem conteúdo"
         print(lista_conteudo)
-        #try:
-        lista_tags=[]
-        spans=discursos.find("div", class_="column").find("div", {"id" : "category"}).find_all("span") 
-        for span in spans:
-            tag = span.find("a", class_="link-category").text
-            lista_tags.append(tag)
-        print(lista_tags)
-        ##print("não possui tags")
 
 def main ():
     """Função principal"""
