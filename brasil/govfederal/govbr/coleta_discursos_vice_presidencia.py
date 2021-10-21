3148aeae2190c8e240775e569993e919c729c197
JLMOTTA <darthmotta@gmail.com>
2021-10-20 21:55:44 -0300

    finalizaçao coleta vice presidencia

---
+++ brasil/govfederal/govbr/coleta_discursos_vice_presidencia.py
@@ -0,0 +1,90 @@
+from io import DEFAULT_BUFFER_SIZE
+from urllib import parse
+from urllib.request import urlopen
+import urllib
+import urllib.request #realizar requisição da página html
+import os #para especificar o caminho do download
+import wget
+import csv
+from tinydb import TinyDB,Query
+from urllib.parse import urlparse #realizar parseamento do html
+from bs4 import BeautifulSoup #importa o beautifulsoup para extrair as infos das tags
+from pprint import pprint #organizar estéticamente os prints
+from requests.models import DecodeError 
+
+DIR_LOCAL= "/home/labri_joaomotta/codigo"
+
+DIR_DADOS= "/media/hdvm10/bd/003/001/001/001/001-b"
+
+def acessar_pagina(url):
+    """Analisa os discursos do site a partir do link"""
+    global response
+    global html
+    response = urllib.request.urlopen(url)
+    html = BeautifulSoup(response, 'lxml', from_encoding = response.info().get_param("charset"))
+    return html
+
+def coleta_link():
+    """Coleta os links de cada página que contém discursos"""
+    lista_links=[]
+    html = acessar_pagina("https://www.gov.br/planalto/pt-br/nova-vice-presidencia/central-de-conteudo/publicacoes/discursos")
+    discursos = html.find_all("article", class_="entry")
+    for articles in discursos:
+        links = articles.a["href"]
+        lista_links.append(links)
+    return lista_links
+
+def coleta_conteudo():
+    """Responsável por coletar título, parágrafo, Tags, atualização e data dos discursos"""
+    db = TinyDB(f"{DIR_LOCAL}/govlatinamerica/brasil/govfederal/govbr/bd/db_discursos_vice.json")
+    User = Query()
+    for link in coleta_link():
+        discursos = acessar_pagina(link)
+        url = link
+        publicado_em = discursos.find("span", class_="documentPublished").find("span", class_="value").text
+        try:
+            atualizado_em = discursos.find("span", class_="documentModified").find("span", class_="value").text
+        except:
+           atualizado_em = "artigo não modificado"
+        try:
+            lista_tags=[]
+            spans=discursos.find("div", {"id" : "category"}).find_all("span") 
+            for span in spans:
+                tag = span.text
+                lista_tags.append(tag)
+        except:
+            lista_tags="não possui tags"
+        titulo = discursos.find("h1", class_="documentFirstHeading").text
+        try:
+            lista_conteudo=[]
+            paragrafos = discursos.find('div', {'id' : 'content-core'}).find_all(['p','h3','h2','h1'])
+            for conteudo in paragrafos:
+                if conteudo.name == 'h3' or conteudo.name == 'h2' or conteudo.name == 'h1':
+                    texto = conteudo.text
+                    texto = texto.upper()
+                else:
+                    texto = conteudo.text 
+                lista_conteudo.append(texto)
+        except:
+            lista_conteudo= "notícia sem conteúdo"
+        db_planalto = db.contains(User.titulo==titulo,User.data==publicado_em)
+        if not db_planalto:
+            print("não está na base")
+            db.insert({
+                "link":url,
+                "data":publicado_em,
+                "atualizado em":atualizado_em,           
+                "tags":lista_tags,
+                "titulo":titulo,
+                "conteudo":lista_conteudo,
+            })
+        else:
+            print("está na base")
+
+
+def main ():
+    """Função principal"""
+    coleta_conteudo()
+
+if __name__ == "__main__":
+    main()
