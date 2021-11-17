c22f99a39081121d51445d21466760201f554287
Cíntia Iório <cintia.iorio@unesp.br>
2021-11-17 12:33:18 -0300

    finalizando coleta do Ministério da Defesa

--- brasil/govfederal/ministerio-defesa/coleta.py
+++ brasil/govfederal/ministerio-defesa/coleta.py
@@ -100,25 +100,141 @@ def receitas(): # check
         list_links_receitas.append(a_receitas["href"])
 
 
-def infos():
+def infos(): # check
     url = "https://www.gov.br/defesa/pt-br/acesso-a-informacao/informacoes-classificadas-e-desclassificadas" 
     md_page = page_access(url)
+    # título
+    title_infos = md_page.find("h1", class_="documentFirstHeading").text
+    # datas
+    post_infos = md_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_infos = md_page.find("span", class_="documentModified").find("span", class_="value").text
+    # conteúdo
+    content_infos = md_page.find("div", {"id":"parent-fieldname-text"}).text
+    # links
+    list_links_infos = []
+    links_infos = md_page.find("div", {"id":"content-core"}).find_all("a")
+    for a_infos in links_infos:
+        list_links_infos.append(a_infos["href"])
 
-def base_dados():
+
+def base_dados(): # check
     url = "https://www.gov.br/defesa/pt-br/acesso-a-informacao/dados-abertos/bases-de-dados-do-ministerio-da-defesa" 
     md_page = page_access(url)
+    # título
+    title_base_dados = md_page.find("h1", class_="documentFirstHeading").text
+    # datas
+    post_base_dados = md_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_base_dados = md_page.find("span", class_="documentModified").find("span", class_="value").text
+    # conteúdo
+    content_base_dados = md_page.find("div", {"id":"parent-fieldname-text"}).text
+    # links
+    list_links_base_dados = []
+    links_base_dados = md_page.find("div", {"id":"content-core"}).find_all("a")
+    for a_base_dados in links_base_dados:
+        list_links_base_dados.append(a_base_dados["href"])
 
-def infograficos():
+
+def infograficos(): # check
     url = "https://www.gov.br/defesa/pt-br/centrais-de-conteudo/publicacoes/infograficos?b_start:int=0" 
     md_page = page_access(url)
+    # título
+    title_infograficos = md_page.find("h1", class_="documentFirstHeading").text
+    # datas
+    post_infograficos = md_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_infograficos = md_page.find("span", class_="documentModified").find("span", class_="value").text
+    counter = 0 
+    list_url_infograficos = []
+    while counter < 21:
+        domain = "https://www.gov.br/defesa/pt-br/centrais-de-conteudo/publicacoes/infograficos?b_start:int="
+        domain += str(counter) 
+        counter += 20
+        list_url_infograficos.append(domain)
+    for url_infograficos in list_url_infograficos:
+        # conteúdo
+        content_infograficos = md_page.find("div", class_="entries").find_all("article")
+        for article_infograficos in content_infograficos:
+            link_infograficos = page_access(article_infograficos.span.a["href"]) 
+            # entrando
+            title_link_infograficos = link_infograficos.find("h1", class_="documentFirstHeading").text
+            post_link_infograficos = link_infograficos.find("span", class_="documentPublished").find("span", class_="value").text
+            update_link_infograficos = link_infograficos.find("span", class_="documentModified").find("span", class_="value").text
+            content_link_infograficos = link_infograficos.find("div", {"id":"parent-fieldname-text"}).text
+            # links
+            try: 
+                list_links_infograficos = []
+                links_infograficos = link_infograficos.find("div", {"id":"content-core"}).find_all("a")
+                for a_infograficos in links_infograficos:
+                    list_links_infograficos.append(a_infograficos["href"])
+            except:
+                pass
+            
 
-def corona():
+def corona(): # check
     url = "https://www.gov.br/defesa/pt-br/centrais-de-conteudo/publicacoes/coronavirus" 
     md_page = page_access(url)
+    # título
+    title_corona = md_page.find("h1", class_="documentFirstHeading").text
+    # datas
+    post_corona = md_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_corona = md_page.find("span", class_="documentModified").find("span", class_="value").text
+    # conteúdo
+    content_corona = md_page.find("div", class_="entries").find_all("article")
+    for article_corona in content_corona:
+        link_corona = page_access(article_corona.span.a["href"]) 
+        # entrando
+        title_link_corona = link_corona.find("h1", class_="documentFirstHeading").text
+        post_link_corona = link_corona.find("span", class_="documentPublished").find("span", class_="value").text
+        update_link_corona = link_corona.find("span", class_="documentModified").find("span", class_="value").text
+        content_link_corona = link_corona.find("div", {"id":"parent-fieldname-text"}).text
+        # links
+        try: 
+            list_links_corona = []
+            links_corona = link_corona.find("div", {"id":"content-core"}).find_all("a")
+            for a_corona in links_corona:
+                list_links_corona.append(a_corona["href"])
+        except:
+            pass
 
-def noticias():
+
+def noticias(): # in progress
     url = "https://www.gov.br/defesa/pt-br/centrais-de-conteudo/noticias" 
     md_page = page_access(url)
+    # título
+    title_noticias = md_page.find("h1", class_="documentFirstHeading").text
+    # datas
+    post_noticias = md_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_noticias = md_page.find("span", class_="documentModified").find("span", class_="value").text
+    counter = 0 
+    list_url_noticias = []
+    while counter < 5941:
+        domain = "https://www.gov.br/defesa/pt-br/centrais-de-conteudo/noticias?b_size=60&b_start:int="
+        domain += str(counter) 
+        counter += 60
+        list_url_noticias.append(domain)
+    for url_noticias in list_url_noticias:
+        # conteúdo
+        content_noticias = md_page.find("ul", class_="noticias listagem-noticias-com-foto").find_all("li")
+        for li_noticias in content_noticias:
+            link_noticias = page_access(li_noticias.div.h2.a["href"]) 
+            # entrando
+            title_link_noticias = link_noticias.find("h1", class_="documentFirstHeading").text
+            try:
+                post_link_noticias = link_noticias.find("span", class_="documentPublished").find("span", class_="value").text
+                update_link_noticias = link_noticias.find("span", class_="documentModified").find("span", class_="value").text
+            except:
+                pass
+            content_link_noticias = link_noticias.find("div", {"id":"parent-fieldname-text"}).text
+            # tags
+            list_tags_noticias = []
+            try: 
+                tags_noticias = link_noticias.find("div", {"id":"category"}).find_all("span")
+                for span_noticias in tags_noticias:
+                    list_tags_noticias.append(span_noticias.text)
+            except:
+                list_tags_noticias.append("Notícia sem tag")
+            if list_tags_noticias[0] != 'Notícia sem tag' :
+                del list_tags_noticias[0]
+
 
 def main():
     global bs
@@ -127,12 +243,12 @@ def main():
     # md_notas = notas()
     # md_artigos = artigos()
     # md_planejamento = planejamento()
-    md_receitas = receitas()
+    # md_receitas = receitas()
     # md_infos = infos()
     # md_base_dados = base_dados()
     # md_infograficos = infograficos()
     # md_corona = corona()
-    # md_noticias = noticias()   
+    md_noticias = noticias()   
 
 
 if __name__ == "__main__":
