ac3879b8a287174077b117493a2d968972299697
Cíntia Iório <cintia.iorio@unesp.br>
2021-12-13 10:18:56 -0300

    iniciando coleta do Ministério da Mulher

--- brasil/govfederal/ministerio-mulher/coleta.py
+++ brasil/govfederal/ministerio-mulher/coleta.py
@@ -9,10 +9,30 @@ def page_access(url):
     return bsoup
 
 
+def infos():
+    url = "https://www.gov.br/mdh/pt-br/acesso-a-informacao/informacoes-classificadas"
+
+
+def dados():
+    url = "https://www.gov.br/mdh/pt-br/acesso-a-informacao/dados-abertos/plano-de-dados-abertos-2020-2022"
+
+
+def publicacoes():
+    url = "https://www.gov.br/mdh/pt-br/centrais-de-conteudo/publicacoes"
+
+
+def noticias():
+    pass
+
+
 def main():
     global bs
-    url = ""
+    url = "https://www.gov.br/mdh/pt-br"
     bs = page_access(url) 
+    mdh_infos = infos()
+    mdh_dados = dados()
+    mdh_publicacoes = publicacoes()
+    mdh_noticias = noticias()
 
 
 if __name__ == "__main__":
