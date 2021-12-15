0c7d7060a717ef3a4c739bda5b52e0692528aa63
Cíntia Iório <cintia.iorio@unesp.br>
2021-12-15 12:38:56 -0300

    finalizando coleta do Ministério do Trabalho - otimizando codigos

--- brasil/govfederal/ministerio-trab/coleta.py
+++ brasil/govfederal/ministerio-trab/coleta.py
@@ -13,29 +13,39 @@ def noticias():
     pass
 
 
-def dados():
+def dados(): # check
     url = "https://www.gov.br/trabalho-e-previdencia/pt-br/acesso-a-informacao/dados-abertos/dados-abertos-previdencia"
     mt_page = page_access(url)
+    title_dados = mt_page.find("h1", class_="documentFirstHeading").text
+    post_dados = mt_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_dados = mt_page.find("span", class_="documentModified").find("span", class_="value").text
+    content_dados = mt_page.find("div", {"id" : "content-core"}).text
+    list_links_dados = []
+    links_dados = mt_page.find("div", class_="conteudo").find_all("a")
+    for a_dados in links_dados:
+        list_links_dados.append(a_dados["href"])
 
 
-def cartilhas(): # in progress
+def cartilhas(): # check
     url = "https://www.gov.br/trabalho-e-previdencia/pt-br/acesso-a-informacao/cartilha-de-emendas-parlamentares-2022"
     mt_page = page_access(url)
     title_cartilhas = mt_page.find("h1", class_="documentFirstHeading").text
     post_cartilhas = mt_page.find("span", class_="documentPublished").find("span", class_="value").text
     update_cartilhas = mt_page.find("span", class_="documentModified").find("span", class_="value").text
-    print(title_cartilhas, post_cartilhas, update_cartilhas)
     content_cartilhas = mt_page.find("div", {"id" : "content-core"}).text
     links_cartilhas = mt_page.find("div", {"id":"content-core"}).find_all("article")
     for article_cartilhas in links_cartilhas:
-        list_links_cartilhas.append(article_cartilhas.a["href"])
-        """
-        fraudes_page = page_access(li_fraudes.a["href"])
-        title_fraudes_page = fraudes_page.find("h1", class_="documentFirstHeading").text
-        post_fraudes_page = fraudes_page.find("span", class_="documentPublished").find("span", class_="value").text
-        update_fraudes_page = fraudes_page.find("span", class_="documentModified").find("span", class_="value").text
-        content_fraudes_page = fraudes_page.find("div", {"id" : "parent-fieldname-text"}).text
-        """
+        cartilhas_page = page_access(article_cartilhas.a["href"])
+        title_cartilhas_page = cartilhas_page.find("h1", class_="documentFirstHeading").text
+        try:
+            post_cartilhas_page = cartilhas_page.find("span", class_="documentPublished").find("span", class_="value").text
+        except:
+            pass
+        update_cartilhas_page = cartilhas_page.find("span", class_="documentModified").find("span", class_="value").text
+        list_links_cartilhas_page = []
+        links_cartilhas_page = cartilhas_page.find("div", {"id":"content-core"}).find_all("a")
+        for a_cartilhas_page in links_cartilhas_page:
+            list_links_cartilhas_page.append(a_cartilhas_page["href"])
 
 
 def acordos(): # check - otimizar
@@ -67,7 +77,7 @@ def acordos(): # check - otimizar
             list_links_acordos_page.append(a_acordos_page["href"])
 
 
-def fraudes(): # check - otimizar
+def fraudes(): # check 
     url = "https://www.gov.br/trabalho-e-previdencia/pt-br/assuntos/assuntos-previdencia/outros/combate-as-fraudes"
     mt_page = page_access(url)
     title_fraudes = mt_page.find("h1", class_="documentFirstHeading").text
@@ -77,10 +87,13 @@ def fraudes(): # check - otimizar
     links_fraudes = mt_page.find("div", class_="content clearfix").find_all("li")
     for li_fraudes in links_fraudes:
         fraudes_page = page_access(li_fraudes.a["href"])
-        title_fraudes_page = fraudes_page.find("h1", class_="documentFirstHeading").text
+        title_fraudes_page = fraudes_page.find("h1", class_="documentFirstHeading").tex
         post_fraudes_page = fraudes_page.find("span", class_="documentPublished").find("span", class_="value").text
         update_fraudes_page = fraudes_page.find("span", class_="documentModified").find("span", class_="value").text
-        content_fraudes_page = fraudes_page.find("div", {"id" : "parent-fieldname-text"}) # contém imagem
+        try:
+            content_fraudes_page = fraudes_page.find("div", {"id" : "parent-fieldname-text"}).find("img")
+        except:
+            content_fraudes_page = fraudes_page.find("div", {"id" : "parent-fieldname-text"}).text
 
 
 def transparencia(): # check
@@ -102,8 +115,8 @@ def main():
     bs = page_access(url) 
     # mt_noticias = noticias()
     # mt_dados = dados()
-    mt_cartilhas = cartilhas()
-    # mt_acordos = acordos()
+    # mt_cartilhas = cartilhas()
+    mt_acordos = acordos()
     # mt_fraudes = fraudes()
     # mt_transparencia = transparencia()
 
