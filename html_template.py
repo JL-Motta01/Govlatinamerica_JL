84248c683a3eaa0b24a2b27c6b3ed23fdfcbd4cd
Cíntia Iório <cintia.iorio@unesp.br>
2022-01-12 09:56:07 -0300

    ajustes no template html; ajuste nas variáveis de ambiente

--- html_template.py
+++ html_template.py
@@ -7,6 +7,8 @@ def diretorio(nome, ano="NA"):
         env_dir = load_dotenv(".env_local") 
         DIR_FINAL = os.getenv("DIR_FINAL")
         MINISTERIO = os.getenv(nome)
+        REFERENCIAS = os.getenv("REFERENCIAS")
+        ESTILO = os.getenv("ESTILO")
         cria_dir_banco = os.makedirs(f'{DIR_FINAL}/{MINISTERIO}/banco', exist_ok = True) # makedirs cria diretório
         cria_dir_html = os.makedirs(f'{DIR_FINAL}/{MINISTERIO}/html', exist_ok = True)
         if ano != "NA":
@@ -16,46 +18,51 @@ def diretorio(nome, ano="NA"):
             dir_html_ano = "NA"
         print(f'{DIR_FINAL}/{MINISTERIO}/banco')
         print(f'{DIR_FINAL}/{MINISTERIO}/html')
-        return (f'{DIR_FINAL}/{MINISTERIO}/banco', f'{DIR_FINAL}/{MINISTERIO}/html', dir_html_ano)
+        return (f'{DIR_FINAL}/{MINISTERIO}/banco', f'{DIR_FINAL}/{MINISTERIO}/html', dir_html_ano, REFERENCIAS, ESTILO)
 
 def consultar():
-    diretorios = diretorio("CIDADANIA")
-    dir_json = diretorios[0]
-    dir_html = diretorios[1]
-    db = TinyDB(f'{dir_json}/CIDADANIA.json')
-    myDBQuery = Query()
-    for dado in iter(db):
-        origem = dado['origem']
-        classificado = dado['classificado']
-        titulo = dado['titulo']
-        subtitulo = dado['subtitulo']
-        link = dado['link']
-        link_archive = dado['link_archive']
-        categoria = dado['categoria']
-        data = dado['data']
-        horario = dado['horario']
-        data_atualizado = dado['data_atualizado']
-        horario_atualizado = dado['horario_atualizado']
-        local = dado['local']
-        autoria = dado['autoria']
-        tags = dado['tags']
-        paragrafos = dado['paragrafos']
-        dir_local = dado['dir_local']
-        extra_01 = dado['extra_01']
-        extra_02 = dado['extra_02']
-        extra_03 = dado['extra_03']
-        print(data)
-        template_html(dir_html, origem, classificado, titulo, subtitulo, link, link_archive, categoria, data, horario, data_atualizado, horario_atualizado, local, autoria, tags, paragrafos, dir_local, extra_01, extra_02, extra_03)
-        
-        print("#################")
-        print("#################")
+    ministerios = ["CIDADANIA", "MMA", "PLANALTO"]
+    for ministerio in ministerios:
+        diretorios = diretorio(ministerio)
+        dir_json = diretorios[0]
+        dir_html = diretorios[1]
+        dir_referencias = diretorios[3]
+        dir_estilo = diretorios[4]
+        db = TinyDB(f'{dir_json}/{ministerio}.json')
+        myDBQuery = Query()
+        for dado in iter(db):
+            origem = dado['origem']
+            classificado = dado['classificado']
+            titulo = dado['titulo']
+            subtitulo = dado['subtitulo']
+            link = dado['link']
+            link_archive = dado['link_archive']
+            categoria = dado['categoria']
+            data = dado['data']
+            horario = dado['horario']
+            data_atualizado = dado['data_atualizado']
+            horario_atualizado = dado['horario_atualizado']
+            local = dado['local']
+            autoria = dado['autoria']
+            tags = dado['tags']
+            paragrafos = dado['paragrafos']
+            dir_local = dado['dir_local']
+            extra_01 = dado['extra_01']
+            extra_02 = dado['extra_02']
+            extra_03 = dado['extra_03']
+            print(data)
+            dir_html_ano = diretorio(ministerio, data[-4:])[2]
+            template_html(dir_html_ano, dir_referencias, dir_estilo, dir_html, origem, classificado, titulo, subtitulo, link, link_archive, categoria, data, horario, data_atualizado, horario_atualizado, local, autoria, tags, paragrafos, dir_local, extra_01, extra_02, extra_03)
+            
+            print("#################")
+            print("#################")
 
-def template_html(dir_html="NA", origem="NA", classificado="NA", titulo="NA", subtitulo="NA", link="NA", link_archive="NA", categoria="NA", data="NA", horario="NA", data_atualizado="NA", horario_atualizado="NA", local="NA", autoria="NA", tags="NA", paragrafos="NA", dir_local="NA", extra_01="NA", extra_02="NA", extra_03="NA"):
+def template_html(dir_html_ano="NA", dir_referencias="NA", dir_estilo="NA", dir_html="NA", origem="NA", classificado="NA", titulo="NA", subtitulo="NA", link="NA", link_archive="NA", categoria="NA", data="NA", horario="NA", data_atualizado="NA", horario_atualizado="NA", local="NA", autoria="NA", tags="NA", paragrafos="NA", dir_local="NA", extra_01="NA", extra_02="NA", extra_03="NA"):
     doc, tag, text = Doc().tagtext()
     paragrafos_avisos = ['Este texto deve ser utilizado somente para fins acadêmicos. Para qualquer outro fim entrar em contato com o respectivo Jornal.', 'O tratamento e disponibilização é realizada com o intuito de facilitar a pesquisa.', 'Não é permitida qualquer atividade com fins lucrativos usando esse texto.']
     links = ["stylesheet"]
-    estilo = "/home/labri_juliasilveira/codigo/template-html/css/style.css"
-    referencias = "/home/labri_juliasilveira/codigo/template-html/js/referencia.js"
+    ESTILO = dir_estilo
+    REFERENCIAS = dir_referencias
     doc.asis('<!DOCTYPE html>')
     with tag('html'):
         with tag('head'):
@@ -63,7 +70,7 @@ def template_html(dir_html="NA", origem="NA", classificado="NA", titulo="NA", su
             for l in links:
                 #doc.asis(f'<link rel={l} type="https://gl.githack.com/unesp-labri/sites/host-css-js/-/raw/master/fsp-css/reset.css">')
                 #doc.asis(f'<link rel={l} type="/home/labri_juliasilveira/codigo/newscloud/template/reset.css">')
-                doc.asis(f'<link rel={l} type="text/css" href={estilo}>')
+                doc.asis(f'<link rel={l} type="text/css" href={ESTILO}>')
                 #doc.asis(f'<link rel={l} type="text/css" href="https://gl.githack.com/unesp-labri/sites/host-css-js/-/raw/master/fsp-css/style.css">')
             doc.asis(f'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">')
             doc.asis(f'<meta name="origem" content="{origem}">') 
@@ -79,7 +86,7 @@ def template_html(dir_html="NA", origem="NA", classificado="NA", titulo="NA", su
             doc.asis(f'<meta name="local" content={local}>') 
             doc.asis(f'<meta name="autoria" content={autoria}>') 
             doc.asis(f'<meta name="tags" content="{tags}">') 
-            doc.asis(f'<meta name="dir_local" content="{dir_local}">') 
+            doc.asis(f'<meta name="dir_local" content="{dir_local}">') # meta tag >> dado sobre os dados / dados sobre a pag.
              
         with tag('body'):
             with tag('div', klass='container'):
@@ -155,11 +162,11 @@ def template_html(dir_html="NA", origem="NA", classificado="NA", titulo="NA", su
                     for paragrafo in paragrafos_avisos:
                         with tag('p', klass='p-footer'):
                             text(paragrafo)
-            doc.asis(f'<script type="text/javascript" src={referencias}></script>')
+            doc.asis(f'<script type="text/javascript" src={REFERENCIAS}></script>')
     # result = indent(doc.getvalue())
     result = doc.getvalue()
 
-    with open(f"{dir_html}/{titulo}.html", "w") as file:
+    with open(f"{dir_html_ano}/{data[-4:]}-{data[3:5]}-{data[:2]}-{horario}-{titulo}.html", "w") as file:
         file.write(result)
 
 
