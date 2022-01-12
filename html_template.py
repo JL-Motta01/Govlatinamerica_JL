c329df82e0baae8336cd51d9289edb730282e5f0
Cintia Iorio <cintia.iorio@unesp.br>
2022-01-12 12:50:46 -0300

    integrando a coleta das noticias com os templates html

--- html_template.py
+++ html_template.py
@@ -2,23 +2,10 @@ from yattag import Doc, indent
 from tinydb import TinyDB, Query
 from dotenv import load_dotenv
 import os
+import sys 
+sys.path.insert(1,"../brasil/govfederal/coleta")
+from coleta_noticias import diretorio 
 
-def diretorio(nome, ano="NA"):
-        env_dir = load_dotenv(".env_local") 
-        DIR_FINAL = os.getenv("DIR_FINAL")
-        MINISTERIO = os.getenv(nome)
-        REFERENCIAS = os.getenv("REFERENCIAS")
-        ESTILO = os.getenv("ESTILO")
-        cria_dir_banco = os.makedirs(f'{DIR_FINAL}/{MINISTERIO}/banco', exist_ok = True) # makedirs cria diretório
-        cria_dir_html = os.makedirs(f'{DIR_FINAL}/{MINISTERIO}/html', exist_ok = True)
-        if ano != "NA":
-            cria_dir_html_ano = os.makedirs(f'{DIR_FINAL}/{MINISTERIO}/html/{ano}', exist_ok = True)
-            dir_html_ano = f'{DIR_FINAL}/{MINISTERIO}/html/{ano}'
-        else:
-            dir_html_ano = "NA"
-        print(f'{DIR_FINAL}/{MINISTERIO}/banco')
-        print(f'{DIR_FINAL}/{MINISTERIO}/html')
-        return (f'{DIR_FINAL}/{MINISTERIO}/banco', f'{DIR_FINAL}/{MINISTERIO}/html', dir_html_ano, REFERENCIAS, ESTILO)
 
 def consultar():
     ministerios = ["CIDADANIA", "MMA", "PLANALTO"]
