dfe2cf4b34290efa07c8b08f690e78e03adc83c0
Cintia Iorio <cintia.iorio@unesp.br>
2022-01-17 10:21:46 -0300

    ajustes no template html

--- brasil/govfederal/coleta/teste.py
+++ brasil/govfederal/coleta/teste.py
@@ -3,8 +3,11 @@ import os
 def teste_acesso():
     print("Você acessou a pasta coleta")
     dir_atual = os.environ["PWD"] # pwd >> mostra a pasta atual em que você está
-    dir_tmp = dir_atual.split("govlatinamerica")
-    dir_root = dir_tmp[0]+"govlatinamerica"
+    lista_dir_atual = dir_atual.split("/")
+    NOME_PROJETO = lista_dir_atual[lista_dir_atual.index("codigo")+1]
+    print(NOME_PROJETO)
+    lista_dir_atual_02 = dir_atual.split(NOME_PROJETO)
+    dir_root = lista_dir_atual_02[0]+NOME_PROJETO
     print(dir_root)
 
 def main():
--- template_html/diretorio.py
+++ template_html/diretorio.py
@@ -29,11 +29,16 @@ def diretorios(nome, ano="NA"):
 def diretorios_template(nome, ano="NA"):
         """ para rodar o template html no computador local, substituir a variável DIR_BD_FINAL por DIR_CONFIG """
         print(f'NOME: {nome}')
-        NOME_PROJETO = "govlatinamerica"
         DIR_ATUAL = os.environ["PWD"] 
-        DIR_TMP = DIR_ATUAL.split("govlatinamerica")
-        DIR_ROOT = DIR_TMP[0]+"govlatinamerica"
-        env_dir = load_dotenv(f'{DIR_ROOT}/template_html/.env_var') 
+        lista_dir_atual = DIR_ATUAL.split("/")
+        NOME_PROJETO = lista_dir_atual[lista_dir_atual.index("codigo")+1]
+        lista_dir_atual_02 = DIR_ATUAL.split(NOME_PROJETO)
+        DIR_ROOT = lista_dir_atual_02[0]+NOME_PROJETO
+        NOME_PROJETO = "govlatinamerica"
+        DIR_TMP = DIR_ATUAL.split(NOME_PROJETO)
+        if NOME_PROJETO != "template_html":
+            DIR_ROOT += "/template_html"
+        env_dir = load_dotenv(f'{DIR_ROOT}/.env_var') 
         LOCAL = os.getenv("LOCAL")
         DIR_BD_FINAL = os.getenv("DIR_BD_FINAL")
         print(f'DIR BD FINAL: {DIR_BD_FINAL}')
