509e4bbdfbdaf1d2aeeb0a5ef800bd92da58fe37
Cintia Iorio <cintia.iorio@unesp.br>
2022-01-20 12:22:09 -0300

    avançando na coleta do MRE

--- brasil/govfederal/mre/coleta.py
+++ brasil/govfederal/mre/coleta.py
@@ -49,15 +49,53 @@ def alertas_consular(): # check
             list_links_alertas_consular_page.append(a_alertas_consular["href"]) 
 
 
-def noticias_consular(): # in progress
+def noticias_consular(): # check
     url = "https://www.gov.br/mre/pt-br/assuntos/portal-consular/alertas%20e%20noticias/noticias/colecao-de-noticias"
     mre_page = page_access(url)
+    title_noticias_consular = mre_page.find("h1", class_="documentFirstHeading").text
+    post_noticias_consular = mre_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_noticias_consular = mre_page.find("span", class_="documentModified").find("span", class_="value").text
+    content_noticias_consular = mre_page.find("div", {"id":"content-core"}).find_all("article")
+    for article_noticias_consular in content_noticias_consular:
+        link_noticias_consular = page_access(article_noticias_consular.div.h2.a["href"]) 
+        title_link_noticias_consular_page = link_noticias_consular.find("h1", class_="documentFirstHeading").text
+        post_link_noticias_consular_page = link_noticias_consular.find("span", class_="documentPublished").find("span", class_="value").text
+        try:
+            update_link_noticias_consular_page = link_noticias_consular.find("span", class_="documentModified").find("span", class_="value").text
+        except:
+            pass
+        content_link_noticias_consular_page = link_noticias_consular.find("div", {"id" : "parent-fieldname-text"}).text
+        list_links_noticias_consular_page = []
+        links_noticias_consular_page = link_noticias_consular.find("div", {"id":"parent-fieldname-text"}).find_all("a")
+        for a_noticias_consular in links_noticias_consular_page:
+            link = a_noticias_consular["href"]
+            pdf = link.split(".")
+            if pdf[-1] == "pdf":
+                list_links_noticias_consular_page.append(a_noticias_consular["href"])
 
 
-def infos_classificadas():
+def infos_classificadas(): # check
     url = "https://www.gov.br/mre/pt-br/acesso-a-informacao/informacoes-classificadas"
     mre_page = page_access(url)
-    
+    title_infos_classificadas = mre_page.find("h1", class_="documentFirstHeading").text
+    post_infos_classificadas = mre_page.find("span", class_="documentPublished").find("span", class_="value").text
+    update_infos_classificadas = mre_page.find("span", class_="documentModified").find("span", class_="value").text
+    content_infos_classificadas = mre_page.find("div", {"id":"content-core"}).find_all("ol")
+    for ol_infos_classificadas in content_infos_classificadas:
+        ol_li_infos_classificadas = ol_infos_classificadas.find_all("li")
+        for li_infos_classificadas in content_infos_classificadas:
+            link_infos_classificadas = page_access(li_infos_classificadas.p.a["href"]) 
+            title_link_infos_classificadas = link_infos_classificadas.find("h1", class_="documentFirstHeading").text
+            post_link_infos_classificadas = link_infos_classificadas.find("span", class_="documentPublished").find("span", class_="value").text
+            try:
+                update_link_infos_classificadas = link_infos_classificadas.find("span", class_="documentModified").find("span", class_="value").text
+            except:
+                pass
+            list_links_infos_classificadas = [] 
+            links_infos_classificadas = link_infos_classificadas.find("div", {"id":"parent-fieldname-text"}).find_all("a")
+            for a_infos_classificadas in links_infos_classificadas:
+                list_links_infos_classificadas.append(a_infos_classificadas["href"])
+        
 
 def dados_abertos():
     url = "https://www.gov.br/mre/pt-br/acesso-a-informacao/dados-abertos"
@@ -96,8 +134,8 @@ def main():
     # mre_noticias = noticias()
     # mre_estudos_mercado_tecnologia = estudos_mercado_tecnologia()
     # mre_alertas_consular = alertas_consular()
-    mre_noticias_consular = noticias_consular()
-    mre_infos_classificadas = infos_classificadas()
+    # mre_noticias_consular = noticias_consular()
+    # mre_infos_classificadas = infos_classificadas()
     mre_dados_abertos = dados_abertos()
     mre_cgrc = cgrc()
     mre_pei_mre = pei_mre()
