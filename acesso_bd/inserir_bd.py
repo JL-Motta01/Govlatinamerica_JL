97c005ccae821f763926ec87883b6ea348f24504
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2022-02-08 12:29:06 -0300

    ajuste nos caminhos indicados nos templates

--- acesso_bd/inserir_bd.py
+++ acesso_bd/inserir_bd.py
@@ -12,14 +12,14 @@ DIR_PROJETO = lista_dir_atual_02[0]+NOME_PROJETO
 sys.path.append(DIR_PROJETO) 
 from templates.diretorios.diretorio import diretorios
 
-def inserir_bd(env_ministerio="NA", origem="NA", classificado="NA", titulo="NA", subtitulo="NA", link="NA", link_archive="NA", data_archive="NA", horario_archive="NA", categoria="NA", data="NA", horario="NA", data_atualizado="NA", horario_atualizado="NA", local="NA", autoria="NA", tags="NA", paragrafos="NA", dir_local="NA", extra_01="NA", extra_02="NA", extra_03="NA"):
-        print(f'ENV MINISTERIO: {env_ministerio}')
-        DIR_FINAL = diretorios(env_ministerio)[0]
+def inserir_bd(dir_bd="NA", origem="NA", classificado="NA", titulo="NA", subtitulo="NA", link="NA", link_archive="NA", data_archive="NA", horario_archive="NA", categoria="NA", data="NA", horario="NA", data_atualizado="NA", horario_atualizado="NA", local="NA", autoria="NA", tags="NA", paragrafos="NA", dir_local="NA", extra_01="NA", extra_02="NA", extra_03="NA"):
+        print(f'ENV MINISTERIO: {dir_bd}')
+        DIR_FINAL = diretorios(dir_bd)
         print(DIR_FINAL)
-        nome_bd_json = env_ministerio 
+        nome_bd_json = dir_bd 
         # excluir_json = os.remove(f'{DIR_FINAL}/{nome_bd_json}.json')
-        db = TinyDB(f'{DIR_FINAL}/{nome_bd_json}.json', indent=4, ensure_ascii = False)
-        dir_local = f'{DIR_FINAL}/{nome_bd_json}.json'
+        db = TinyDB(f'{DIR_FINAL}/json/{nome_bd_json}.json', indent=4, ensure_ascii = False)
+        dir_local = f'{DIR_FINAL}/json/{nome_bd_json}.json'
         User = Query()
         verifica_bd = db.contains((User.titulo == titulo)&(User.data == data)&(User.horario == horario))
         print(verifica_bd)
@@ -55,7 +55,7 @@ def inserir_bd(env_ministerio="NA", origem="NA", classificado="NA", titulo="NA",
             pass
 
 def main():
-    inserir_banco = inserir_bd(env_ministerio, origem, classificado, titulo, subtitulo, link, link_archive, categoria, data, horario, data_atualizado, horario_atualizado, local, autoria, tags, paragrafos, dir_local, extra_01, extra_02, extra_03)
+    inserir_banco = inserir_bd(dir_bd, origem, classificado, titulo, subtitulo, link, link_archive, categoria, data, horario, data_atualizado, horario_atualizado, local, autoria, tags, paragrafos, dir_local, extra_01, extra_02, extra_03)
 
 if __name__=="__main__":
     main()
--- diretorios/diretorio.py
+++ diretorios/diretorio.py
@@ -12,7 +12,7 @@ def diretorios(nome, ano="NA"):
         lista_dir_atual_02 = DIR_ATUAL.split(NOME_PROJETO)
         DIR_ROOT = lista_dir_atual_02[0]+NOME_PROJETO
         env_dir = load_dotenv(f'{DIR_ROOT}/.env_dir') 
-        DIR_BD_FINAL = os.getenv("DIR_BD_FINAL")
+        DIR_BD_FINAL = os.getenv(nome)
         print(f'DIR BD FINAL: {DIR_BD_FINAL}')
         BASE_DADO = os.getenv(str(nome))
         print(f'BASE_DADO: {BASE_DADO}')
@@ -22,13 +22,13 @@ def diretorios(nome, ano="NA"):
             DIR_TEMPLATE_HTML = DIR_ROOT + "/templates/template_html"
             ESTILO = DIR_TEMPLATE_HTML + "/css/style.css" 
             REFERENCIAS = DIR_TEMPLATE_HTML + "/js/referencia.js" 
-        cria_dir_banco = os.makedirs(f'{DIR_BD_FINAL}/{BASE_DADO}/banco', exist_ok = True) # makedirs cria diretório
-        DIR_BD = f'{DIR_BD_FINAL}/{BASE_DADO}/banco'
-        cria_dir_html = os.makedirs(f'{DIR_BD_FINAL}/{BASE_DADO}/html', exist_ok = True)
-        DIR_HTML = f'{DIR_BD_FINAL}/{BASE_DADO}/html'
+        cria_dir_banco = os.makedirs(f'{DIR_BD_FINAL}/json', exist_ok = True) # makedirs cria diretório
+        DIR_BD = f'{DIR_BD_FINAL}/banco'
+        cria_dir_html = os.makedirs(f'{DIR_BD_FINAL}/html', exist_ok = True)
+        DIR_HTML = f'{DIR_BD_FINAL}/html'
         if ano != "NA":
-            cria_dir_html_ano = os.makedirs(f'{DIR_BD_FINAL}/{BASE_DADO}/html/{ano}', exist_ok = True)
-            DIR_HTML_ANO = f'{DIR_BD_FINAL}/{BASE_DADO}/html/{ano}'
+            cria_dir_html_ano = os.makedirs(f'{DIR_BD_FINAL}/html/{ano}', exist_ok = True)
+            DIR_HTML_ANO = f'{DIR_BD_FINAL}/html/{ano}'
         else:
             DIR_HTML_ANO = "NA"
         return (DIR_BD, DIR_HTML, DIR_HTML_ANO, REFERENCIAS, ESTILO)
@@ -49,20 +49,18 @@ def diretorios_template(nome, ano="NA"):
             ESTILO = DIR_ROOT + "/css/style.css" 
             REFERENCIAS = DIR_ROOT + "/js/referencia.js"       
         print(f'{DIR_ROOT}/.env_dir')
-        DIR_BD_FINAL = os.getenv("DIR_BD_FINAL")
-        DIR_BD_FINAL2 = os.getenv("DIR_BD_FINAL2")
+        DIR_BD_FINAL = os.getenv(nome)
         print(f'DIR BD FINAL: {DIR_BD_FINAL}')
         BASE_DADO = os.getenv(str(nome))
         print(f'BASE_DADO: {BASE_DADO}')
-        cria_dir_banco = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/banco', exist_ok = True) # makedirs cria diretório
-        # /home/labri_cintiaiorio/codigo/govlatinamerica/template_html/exemplos/min-cidadania/banco/CIDADANIA.json
-        cria_dir_html = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/html', exist_ok = True)
+        cria_dir_banco = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/json', exist_ok = True)
+        cria_dir_html = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/html', exist_ok = True)
         if ano != "NA":
-            cria_dir_html_ano = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/html/{ano}', exist_ok = True)
-            dir_html_ano = f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/html/{ano}'
+            cria_dir_html_ano = os.makedirs(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/html/{ano}', exist_ok = True)
+            dir_html_ano = f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/html/{ano}'
         else:
             dir_html_ano = "NA"
-        print(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/banco')
-        print(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/html')
-        return (f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/banco', f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/html', dir_html_ano, REFERENCIAS, ESTILO)
+        print(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/json')
+        print(f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/{BASE_DADO}/json')
+        return (f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/json', f'{DIR_ROOT}/exemplos/{NOME_PROJETO}/html', dir_html_ano, REFERENCIAS, ESTILO)
 
