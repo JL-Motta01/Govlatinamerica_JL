7f06069066f14fc0e3003a0c8a4c70c52a69f727
Treyce Annunciado <treyce.annunciatohr1@gmail.com>
2022-03-23 12:49:42 -0300

    estruturando coleta das notas de imprensa atuais

--- brasil/govfederal/mre/notas_imprensa_atual.py
+++ brasil/govfederal/mre/notas_imprensa_atual.py
@@ -11,7 +11,15 @@ def extrair_info():
     bs = acessar_pagina("https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa/")
     tag_h1 = bs.find("h1").text
     print(tag_h1)
-
+    notas = bs.find_all("article")
+    #print(notas)
+    for nota in notas:
+        titulo = nota.find("h2").text
+        data = nota.find_all("span", class_="summary-view-icon")[0].text.strip()
+        print(titulo)
+        print(data)
+#//*[@id="content-core"]/article[1]/div/h2/a
+#informações importantes: n° da nota, título, data, horário e link para conteúdo
 
 def main():
     extrair_info()
